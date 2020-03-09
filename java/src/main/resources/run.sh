a5284c8b2b6032c4ee9075cfe5dde92c128af197
nicolae-leonte-go <42137924+nicolae-leonte-go@users.noreply.github.com>
2020-03-09 17:47:18 +0000

    use exec in plugin shell script (#1)
    use exec in the java plugin run.sh script so that the go-plugin process stop signal goes to the java process

--- java/src/main/resources/run.sh
+++ java/src/main/resources/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-/usr/local/bin/java -jar ${project.build.finalName}.jar
+# For the time being use exec in order to cope with hashicorp's go-plugin failure to kill child processes.
+# See https://github.com/hashicorp/go-plugin/issues/136
+exec /usr/local/bin/java -jar ${project.build.finalName}.jar
